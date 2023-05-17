fc14e39a300aa9a91bb3e1e345de76fc9872e0c0
DvdMeneses <david.meneses.cc@gmail.com>
2023-05-16 22:51:45 -0300

    NOVOS TESTS

--- queries_Turma.py
+++ queries_Turma.py
@@ -11,3 +11,14 @@ def ler_Alunos_Turma(bd, id_Turma):
 
     return ler_bd(bd, query, (id_Turma,))
 
+def ler_Medias_Turma(bd,codigo_turma):
+    #Mostrar media de todos os alunos de uma turma atraves do codigo
+
+    query = "SELECT m.media " \
+            "FROM Aluno a, Media_turma_aluno m, Turma t " \
+            "WHERE m.id_aluno = a.id AND " \
+            "m.id_turma = t.id AND " \
+            "t.codigo = ?"
+
+    return ler_bd(bd, query, (codigo_turma,))
+
--- tests/test_Turma_bd.py
+++ tests/test_Turma_bd.py
@@ -1,7 +1,7 @@
 from MockDb import MockBD
 import sys
 
-from queries_Turma import ler_Alunos_Turma
+from queries_Turma import ler_Alunos_Turma, ler_Medias_Turma
 
 sys.path.insert(0, '..')
 sys.path.insert(0, '../tests')
@@ -17,4 +17,15 @@ class TestTurmaBD(MockBD):
 
         self.assertEqual(ler_Alunos_Turma(self.mock_db_config.get('bd'), turma1), retorno)
 
-  
+    def test_MediasDaTurma(self):
+        #medias da turma TAD0203
+        turma1 = 'TAD0203'
+        retorno = [(10,), (9.5,), (9.0,), (10,), (10,)]
+
+        self.assertEqual(ler_Medias_Turma(self.mock_db_config.get('bd'), turma1), retorno)
+
+
+
+
+
+
